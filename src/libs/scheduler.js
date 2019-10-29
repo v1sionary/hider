3a04fa6820fdbacfc0cc3d994d37db33c767c294
jinbiao chen <jinbiaochen@jinbiaodeMacBook-Pro.local>
2019-10-29 13:49:20 +0800

    feat sweep all in main view & todo scheduler

---
+++ src/libs/scheduler.js
@@ -0,0 +1,11 @@
+// https://developer.chrome.com/apps/alarms
+
+export function createTask(opt) {}
+
+export function deleteTask(id) {}
+
+export function getTask(id) {}
+
+export function getTaskList() {}
+
+export function clearAllTasks() {}
--- src/options/pages/OptionsMain.vue
+++ src/options/pages/OptionsMain.vue
@@ -4,6 +4,7 @@
       <router-link to="/edit/">
         <el-button type="primary" plain size="small" icon="el-icon-plus">新增</el-button>
       </router-link>
+      <el-button class="sweep-all" type="text" size="small" icon="el-icon-magic-stick" @click="sweepAll">执行所有任务</el-button>
     </div>
     <el-row>
       <el-col class="rule-container" :span="24" v-if="rules.length === 0">暂无数据</el-col>
@@ -18,6 +19,7 @@
 import Vue from 'vue';
 import { Row, Col } from 'element-ui';
 import RuleBoard from '../components/RuleBoard';
+import { sweepByRule } from '../../libs/sweeper';
 
 Vue.use(Row);
 Vue.use(Col);
@@ -43,6 +45,40 @@ export default {
       const index = this.rules.findIndex(rule => rule.id === id);
       this.rules.splice(index, 1);
     },
+    sweepAll() {
+      this.$confirm('确定执行所有清除任务吗？', '警告！', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning',
+      })
+        .then(() => {
+          this.rules
+            .filter(r => r.enabled)
+            .forEach(r => {
+              sweepByRule(r).then(
+                removeCount => {
+                  this.$message({
+                    showClose: true,
+                    message: `成功清除 - ${r.keyword}，清除条数：${removeCount}`,
+                    type: 'success',
+                    duration: 1500,
+                  });
+                },
+                e => {
+                  setTimeout(() => {
+                    this.$message({
+                      showClose: true,
+                      message: `清除失败 - ${r.keyword}，${e}`,
+                      type: 'error',
+                      duration: 0,
+                    });
+                  });
+                }
+              );
+            });
+        })
+        .catch(e => {});
+    },
   },
 };
 </script>
@@ -50,5 +86,9 @@ export default {
 <style scoped>
 .rule-container {
   padding: 3px;
+}
+
+.sweep-all {
+  margin-left: 7px;
 }
 </style>
